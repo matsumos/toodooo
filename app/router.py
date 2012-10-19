ff5c4d85f067bbd4e1c452d9964b371ae04a39d4
matsumos <matsumos@gmail.com>
2012-10-19 21:12:53 +0900

    init

---
+++ app/router.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from shimehari import Router, Resource
+from controllers import IndexController
+
+#
+# You can routing like Rails style.
+#
+# from shimehari.routing import RESTfulRouter
+# appRoutes = RESTfulRouter([
+#    Resource(IndexController,root=True),
+#    Resource(SomeOtherController),
+#    Resource(SomethingElseController, expect=['edit', 'destroy'])
+#])
+
+
+appRoutes = Router([
+    Resource(IndexController, root=True)
+])
+
+#
+# You can routing like werkzeug style.
+#
+# from shimehari import BasicRouter
+# appRoutes = BasicRouter([
+#    {'/': IndexController.list},
+#    {'/hoge': SomeOtherController.list},
+#    {'/hoge/<int:id>': SomeOtherController.show}
+#])
